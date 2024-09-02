f362d7dc3fae51fafac43d2cd666453a4aeb6934
Alexey Alexin <lexin13@users.noreply.github.com>
2024-09-02 17:21:33 +0300

    Add information about yourself

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,9 +7,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
-        </p>
+        <h3>
+          Алехин Алексей
+        </h3>
+        <ul>
+          <li>
+            Опыт работы 18 лет, сопровождение и разработка Enterprise проекта ориентированного на учет персонала, написание функций и хранимых процедур для SQL Server, оптимизация SQL запросов, разработака отчетов
+          </li>
+          <li>
+            Цель обучения: пополнение и систематизация знаний о React для дальнейшего применения в рабочем проекте
+            Технологии: Delphi, MS SQL Server, SQL, FastReports, Git; знаком с C#, JS
+          </li>
+        </ul>
       </header>
     </div>
   );
