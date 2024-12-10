ab4f5e22574092d2406ca695fec7402f06748d25
Alexey Alexin <lexin13@users.noreply.github.com>
2024-12-10 18:46:37 +0300

    wip

--- src/shared/common/Tip/Tip.stories.tsx
+++ src/shared/common/Tip/Tip.stories.tsx
@@ -1,14 +1,25 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import style from "./TipStories.module.scss"
 
 import { Tip } from './Tip';
 import React from 'react';
 
 const TipWrapper = () => (
-    <div>
-        <Tip title="ttile1">
-            <div style={{ position: 'absolute', width: 40, height: 40, top: 60, left: 10, backgroundColor: '#efc' }}>
-                test
-            </div>
+    <div className={style["test-elements"]}>
+        <Tip title="This is a button">
+            <button className={style["element"]}>Button</button>
+        </Tip>
+        <Tip title="This is an input field">
+            <input className={style["element"]} type="text" placeholder="Input field" />
+        </Tip>
+        <Tip title="This is a checkbox">
+            <input className={style["element"]} type="checkbox" /> Checkbox
+        </Tip>
+        <Tip title="This is a link">
+            <a className={style["element"]} href="#">Link</a>
+        </Tip>
+        <Tip title="This is a paragraph">
+            <p className={style["element"]}>Paragraph text</p>
         </Tip>
     </div>
 )
@@ -27,49 +38,3 @@ export const Sample = {
         name: 'Product Name',
     },
 };
-
-// const Wrapper = () => (
-//   <ComponentInfo title="Tip" desc={`Компонент использует useLayoutEffect и onTransitionEnd.`}>
-//     <div style={{ position: 'relative', width: 400, height: 1200, overflow: 'hidden' }}>
-//       <Tip title="Текст получил широкое распространение в учебных и демонстрационных материалах по полиграфии и дизайну печатных изданий и веб-страниц. Распространился в 1970-х годах из-за трафаретов компании Letraset, а затем — из-за того, что служил примером текста в программе PageMaker. Применяется для того, чтобы показать читателю (пользователю) как будет выглядеть страница при использовании того или иного набора оформительских элементов и шрифтов. Бессмысленность текста приводит к тому, что при этом читатель сосредотачивается именно на внешнем виде и общем восприятии страницы, а не на ее содержании">
-//         <div style={{ height: 50, top: 60, left: 10, backgroundColor: '#efc' }}>test</div>
-//       </Tip>
-//       <Tip title="ttile1">
-//         <span style={{ position: 'absolute', width: 40, height: 40, top: 60, left: 10, backgroundColor: '#efc' }}>
-//           test
-//         </span>
-//         {/*<div></div>*/}
-//       </Tip>
-//       <Tip title="ttile1">
-//         <div style={{ position: 'absolute', width: 40, height: 20, top: 60, left: 80, backgroundColor: '#efc' }}></div>
-//       </Tip>
-//       <Tip title="ttile1">
-//         <div style={{ position: 'absolute', width: 40, height: 40, top: 60, left: 160, backgroundColor: '#efc' }}>
-//           test
-//         </div>
-//       </Tip>
-//       <Tip title="ttile1">
-//         <div style={{ position: 'absolute', width: 40, height: 80, top: 60, left: 240, backgroundColor: '#efc' }}>
-//           <div>
-//             <div>2323</div>
-//           </div>
-//         </div>
-//       </Tip>
-//       <Tip title={<div>sdqwd32</div>}>
-//         <div style={{ position: 'absolute', width: 40, height: 40, top: 230, left: 10, backgroundColor: '#efc' }}>
-//           test
-//         </div>
-//       </Tip>
-//     </div>
-//   </ComponentInfo>
-// );
-
-// const meta: Meta<typeof Tip> = {
-//   title: 'Tip',
-//   component: Wrapper,
-// };
-
-// export default meta;
-// type Story = StoryObj<typeof meta>;
-
-// export const Default = {};
--- src/shared/common/Tip/Tip.tsx
+++ src/shared/common/Tip/Tip.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import style from "./Tip.module.scss";
 import { createPortal } from "react-dom";
 
@@ -7,11 +7,26 @@ interface TipProps extends React.PropsWithChildren<{}> {
 }
 
 export const Tip: React.FC<TipProps> = ({ title, children }) => {
-    return createPortal(
-        <div className={style.tip}>
-            <div className={style.title}>{title}</div>
-            {children}
-        </div>,
-        document.body
-    );
+    const container = React.useRef<HTMLDivElement>(null);
+    const [visible, setVisible] = React.useState(false);
+    const showTip = () => setVisible(true);
+    const hideTip = () => setVisible(false);
+
+    useLayoutEffect(() => {
+
+    }, [visible]);
+
+    return (
+        <div className={style["tip-container"]} ref={container}>
+            {visible && createPortal(
+                <div className={style.tip}>
+                    <div className={style.title}>{title}</div>
+                </div>,
+                document.body
+            )}
+            <div onMouseEnter={showTip} onMouseLeave={hideTip}>
+                {children}
+            </div>
+        </div>
+    )
 };
