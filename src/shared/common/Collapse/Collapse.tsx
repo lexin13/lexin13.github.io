c3f857009d97284d7d4eeafd8c1ca910cc1e42fd
Lexin <lexintmp@gmail.com>
2024-12-15 18:11:02 +0300

    wip

--- src/shared/common/Collapse/Collapse.tsx
+++ src/shared/common/Collapse/Collapse.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import style from './collapse.module.scss';
+import cn from 'clsx';
 
 export interface CollapseProps extends React.PropsWithChildren<{}> {
-    visible?: boolean
+    opened: boolean
 }
 
-const Collapse: React.FC<CollapseProps> = ({ children, visible }) => {
-    return (visible &&
-        <div className="collapse">
+export interface CollapseProps extends React.PropsWithChildren<{}> {
+    opened: boolean
+}
+
+export const Collapse: React.FC<CollapseProps> = ({ children, opened }) => {
+
+    const [mounted, setMounted] = React.useState(true);
+
+    const onTransitionEnd = (event: React.TransitionEvent) => {
+        console.log('Transition finished on', event.propertyName);
+        !opened && setMounted(false);
+    };
+
+    opened && !mounted && setMounted(true);
+
+    console.log(opened, mounted);
+
+    return (mounted &&
+        <div className={cn(style.collapse, { [style.hidden]: !opened })} onTransitionEnd={onTransitionEnd}>
             {children}
         </div>
     );
 }
+
